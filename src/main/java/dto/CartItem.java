95d9679ca742247893dd644aaab1639a53b48f7d
nisalmissaka <wishwakanisal@gmail.com>
2025-12-25 18:51:27 +0530

    create to CartItem.java class

--- src/main/java/dto/CartItem.java
+++ src/main/java/dto/CartItem.java
@@ -1,4 +1,19 @@
 package dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
+
+    private String itemCode;
+    private String description;
+    private int quantity;
+    private double unitPrice;
+    private double discount;
+    private double total;
 }
+
