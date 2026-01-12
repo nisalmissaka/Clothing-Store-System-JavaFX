66b56366d8b0df03de2d3e3b5ebf8abaea92cc14
nisalmissaka <wishwakanisal@gmail.com>
2026-01-12 18:36:05 +0530

    git commit -m "Define PlaceOrderService interface with order, item, and customer methods"

--- src/main/java/service/PlaceOrderService.java
+++ src/main/java/service/PlaceOrderService.java
@@ -1,7 +1,10 @@
 package service;
 
 import dto.CartItem;
+import dto.Customer;
+import dto.Item;
 import dto.Order;
+import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -9,6 +12,13 @@ import java.util.List;
 public interface PlaceOrderService {
 
     boolean placeOrder(Order order, List<CartItem> cartItems) throws SQLException;
+
+    boolean placeOrder(Order order, ObservableList<CartItem> cartItemObservableList) throws SQLException;
+
+    Item searchItem(String txtItemCode);
+
+    Customer searchCustomer(String customerId);
+
 }
 
 
