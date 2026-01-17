88c86734d4a089dee3af5d286f0dc7abe86d46fb
nisalmissaka <wishwakanisal@gmail.com>
2026-01-17 16:31:49 +0530

    git commit -m "change to private variable"

--- src/main/java/dto/Item.java
+++ src/main/java/dto/Item.java
@@ -14,14 +14,14 @@ public class Item {
     private String itemCode;
     private String description;
     private String itemSize;
-    private double itemPrice;
+    private double itemprice;
     private double discount;
     private int quantity;
 
-    public Item(String itemCode, String description, double itemPrice, double discount) {
+    public Item(String itemCode, String description, double itemprice, double discount) {
         this.itemCode = itemCode;
         this.description = description;
-        this.itemPrice = itemPrice;
+        this.itemprice = itemprice;
         this.discount = discount;
     }
 
