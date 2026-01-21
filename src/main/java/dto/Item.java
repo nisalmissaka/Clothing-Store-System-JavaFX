5b9ab4172080c6132ef028c4a6945184dfb49d54
nisalmissaka <wishwakanisal@gmail.com>
2026-01-21 21:22:04 +0530

    git commit -m " Fix  item data loading from database
    "

--- src/main/java/dto/Item.java
+++ src/main/java/dto/Item.java
@@ -14,19 +14,16 @@ public class Item {
     private String itemCode;
     private String description;
     private String itemSize;
-    private double itemprice;
+    private double itemPrice;
     private double discount;
     private int quantity;
 
-    public Item(String itemCode, String description, double itemprice, double discount) {
+    public Item(String itemCode, String description, double itemPrice, double discount,int quantity) {
         this.itemCode = itemCode;
         this.description = description;
-        this.itemprice = itemprice;
+        this.itemPrice = itemPrice;
         this.discount = discount;
-    }
-
-    public Item(String txtItemCode, String description, double itemprice, double discount, int quantity) {
-
+        this.quantity = quantity;
     }
 }
 
