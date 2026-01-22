dbfba73250188c3c42f64f2dfeaf7d4e097b60ff
nisalmissaka <wishwakanisal@gmail.com>
2026-01-22 12:52:29 +0530

    git commit -m "Added toString method
    "

--- src/main/java/dto/CartItem.java
+++ src/main/java/dto/CartItem.java
@@ -3,10 +3,12 @@ package dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class CartItem {
 
     private String itemCode;
