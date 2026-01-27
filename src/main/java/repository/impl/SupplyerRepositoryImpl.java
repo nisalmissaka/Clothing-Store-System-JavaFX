abf8fd660dc11a18823fda89b87de7d74068496e
nisalmissaka <wishwakanisal@gmail.com>
2026-01-27 18:54:07 +0530

    change to getSupplyerBrand and get SupplyerEmail

--- src/main/java/repository/impl/SupplyerRepositoryImpl.java
+++ src/main/java/repository/impl/SupplyerRepositoryImpl.java
@@ -12,13 +12,13 @@ public class SupplyerRepositoryImpl implements SupplyerRepository {
     @Override
     public void AddSupplyer(Supplyer supplyer) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothing_store_db","root","nisal");
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO suppliers VALUES(?,?,?,?,?,?)");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO suppliers (Supplier_ID, Supplier_Name, Company_Name, Contact_Number, Supplier_Email, Supplier_Brand) VALUES (?, ?, ?, ?, ?, ?)");
         preparedStatement.setObject(1,supplyer.getSupplyerID());
         preparedStatement.setObject(2,supplyer.getSupplyerName());
         preparedStatement.setObject(3,supplyer.getCompanyName());
         preparedStatement.setObject(4,supplyer.getContactNumber());
-        preparedStatement.setObject(5,supplyer.getSupplyerEmail());
-        preparedStatement.setObject(6,supplyer.getSupplyerBrand());
+        preparedStatement.setObject(5,supplyer.getSupplyerBrand());
+        preparedStatement.setObject(6,supplyer.getSupplyerEmail());
 
         preparedStatement.executeUpdate();
     }
