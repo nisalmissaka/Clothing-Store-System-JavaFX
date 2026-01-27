6dd4fc5de860da4dfb746f741d5af0e80996a852
nisalmissaka <wishwakanisal@gmail.com>
2026-01-27 18:00:11 +0530

    add to supplyer INSERT quary

---
+++ src/main/java/repository/impl/SupplyerRepositoryImpl.java
@@ -0,0 +1,25 @@
+package repository.impl;
+
+import dto.Supplyer;
+import repository.SupplyerRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SupplyerRepositoryImpl implements SupplyerRepository {
+    @Override
+    public void AddSupplyer(Supplyer supplyer) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothing_store_db","root","nisal");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO suppliers VALUES(?,?,?,?,?,?)");
+        preparedStatement.setObject(1,supplyer.getSupplyerID());
+        preparedStatement.setObject(2,supplyer.getSupplyerName());
+        preparedStatement.setObject(3,supplyer.getCompanyName());
+        preparedStatement.setObject(4,supplyer.getContactNumber());
+        preparedStatement.setObject(5,supplyer.getSupplyerEmail());
+        preparedStatement.setObject(6,supplyer.getSupplyerBrand());
+
+        preparedStatement.executeUpdate();
+    }
+}
