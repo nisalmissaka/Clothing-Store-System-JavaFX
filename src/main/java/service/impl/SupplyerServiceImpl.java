7661d83b41aeeff6624c4ebf87673cb0703797c2
nisalmissaka <wishwakanisal@gmail.com>
2026-01-27 17:41:19 +0530

    create to addsupplyer method SupplyerServiceImpl.java class

--- src/main/java/service/impl/SupplyerServiceImpl.java
+++ src/main/java/service/impl/SupplyerServiceImpl.java
@@ -1,9 +1,20 @@
 package service.impl;
 
+import dto.Supplyer;
+import repository.SupplyerRepository;
+import service.SupplyerService;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public abstract class SupplyerServiceImpl {
+public class SupplyerServiceImpl implements SupplyerService {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    public abstract void initialize(URL url, ResourceBundle resourceBundle);
+    }
+
+    @Override
+    public void AddSupplyer(Supplyer supplyer) {
+
+    }
 }
