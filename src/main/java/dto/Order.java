c5c5c0a300c3c785809efccc446ee26f0d656ff7
nisalmissaka <wishwakanisal@gmail.com>
2026-02-08 19:59:18 +0530

    change to Order.java class

--- src/main/java/dto/Order.java
+++ src/main/java/dto/Order.java
@@ -8,13 +8,12 @@ import lombok.*;
 @AllArgsConstructor
 public class Order {
 
+    private String orderID;
+    private String sellerId;
+    private double totalAmount;
+    private String status;
+    private String orderDate;
 
-    private String itemCode;
-    private String description;
-    private int quantity;
-    private double unitPrice;
-    private double discount;
-    private double total;
 }
 
 
