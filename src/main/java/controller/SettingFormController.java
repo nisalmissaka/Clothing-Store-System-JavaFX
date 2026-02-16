2941975fff8cd1be838c76a991604e85f6dd3c25
nisalmissaka <wishwakanisal@gmail.com>
2026-02-16 13:16:03 +0530

    create to setting_form.fxml file

---
+++ src/main/java/controller/SettingFormController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class SettingFormController {
+
+    @FXML
+    private ComboBox<?> cmbAsiaColombo;
+
+    @FXML
+    private ComboBox<?> cmbLRK;
+
+    @FXML
+    private ComboBox<?> cmbphoneno;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnOnActionBrowse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionDiscardChanges(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionSaveSetting(ActionEvent event) {
+
+    }
+
+}
